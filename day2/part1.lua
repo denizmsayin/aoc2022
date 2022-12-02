40509344e6a2d6294d7618d77ffe1dc9358545da
Deniz Mercadier Sayin <denizmsayin@gmail.com>
2022-12-02 12:19:16 +0100

    day 2 complete... why is string indexing so hard?

---
+++ day2/part1.lua
@@ -0,0 +1,29 @@
+-- rock 0, paper 1, scissors 2
+
+function get_match_score(a, b)
+    if a == b then
+        return 3
+    elseif (a - 1) % 3 == b then
+        return 6
+    else
+        return 0
+    end
+end
+
+-- What the hell is going on here... Why is everything so hard!?
+function numcvt(letter, base_letter)
+    return string.byte(letter, 1) - string.byte(base_letter, 1)
+end
+
+total_score = 0
+repeat 
+    l = io.read()
+    if l then
+        me = numcvt(string.sub(l, 3, 3), 'X')
+        other = numcvt(string.sub(l, 1, 1), 'A')
+        match_score = get_match_score(me, other)
+        choice_score = me + 1
+        total_score = total_score + match_score + choice_score
+    end
+until l == nil
+print(total_score)
