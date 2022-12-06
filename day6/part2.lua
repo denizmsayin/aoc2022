11eb0f3b7eee6fe1ba2aff6a8282ef99e6f9928a
Deniz Mercadier Sayin <denizmsayin@gmail.com>
2022-12-06 10:36:05 +0100

    day6

---
+++ day6/part2.lua
@@ -0,0 +1,40 @@
+K = 14
+
+function counter_add(counter, k)
+    if counter[k] == nil then
+        counter[k] = 1
+    else
+        counter[k] = counter[k] + 1
+    end
+end
+
+function counter_sub(counter, k)
+    if counter[k] == nil or counter[k] <= 0 then
+        error('Counter logic error')
+    end
+    counter[k] = counter[k] - 1
+end
+
+function counter_size(counter)
+    local s = 0
+    for _, v in pairs(counter) do
+        if v > 0 then
+            s = s + 1
+        end
+    end
+    return s
+end
+
+stream = io.read()
+counter = {}
+for i = 1, #stream do
+    counter_add(counter, stream:sub(i, i))
+    if i > K then
+        counter_sub(counter, stream:sub(i-K, i-K))
+    end
+    if counter_size(counter) == K then
+        print(i)
+        break
+    end
+end
+
