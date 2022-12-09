4607176e91a106d1751ccfe5cccf1a616c51c533
Deniz Mercadier Sayin <denizmsayin@gmail.com>
2022-12-09 06:29:01 +0100

    day 9 done

--- day9.lua
+++ day9.lua
@@ -1,5 +1,7 @@
 local utils = require('lib/utils')
 
+NUM_TAILS = utils.is_part_1() and 1 or 9 -- different tail size depending on part
+
 local function sign(x) return x > 0 and 1 or -1 end
 
 local function follow_step(ti, tj, hi, hj)
@@ -25,6 +27,17 @@ end
 
 local function encode_coord(i, j) return tostring(i) .. ',' .. tostring(j) end
 
+local function follow_steps(tail_positions, hi, hj, visited)
+    for i = 1, #tail_positions do
+        local ti, tj = table.unpack(tail_positions[i])
+        ti, tj = follow_step(ti, tj, hi, hj)
+        tail_positions[i] = { ti, tj }
+        hi, hj = ti, tj
+    end
+    local ti, tj = table.unpack(tail_positions[#tail_positions])
+    visited[encode_coord(ti, tj)] = true
+end
+
 local function table_size(table)
     local size = 0
     for _, v in pairs(table) do
@@ -35,17 +48,23 @@ local function table_size(table)
     return size
 end
 
+local function create_tails(size)
+    local tails = {}
+    for _ = 1, size do table.insert(tails, { 0, 0 }) end
+    return tails
+end
+
 local offsets = { R = { 0, 1 }, L = { 0, -1 }, U = { -1, 0 }, D = { 1, 0 } }
-local hi, hj, ti, tj = 0, 0, 0, 0
-local visited = { [encode_coord(ti, tj)] = true }
+local hi, hj = 0, 0
+local tail_positions = create_tails(NUM_TAILS)
+local visited = { [encode_coord(0, 0)] = true }
 for line in io.lines() do
     local dir, steps = line:match('(%a) (%d+)')
     steps = tonumber(steps)
     local ioff, joff = table.unpack(offsets[dir])
     for _ = 1, steps do
         hi, hj = hi + ioff, hj + joff
-        ti, tj = follow_step(ti, tj, hi, hj)
-        visited[encode_coord(ti, tj)] = true
+        follow_steps(tail_positions, hi, hj, visited)
     end
 end
 print(table_size(visited))
